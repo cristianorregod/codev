8b986c3261d7e62b2a8b9e59b2cd800a7033bacc
cristianorregod <cristian.duquew@gmail.com>
2025-05-01 21:28:19 -0500

    Feat: Agrega funcionalidad de copiar al clipboard

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,41 +1,45 @@
-import { getPostBySlug, getPosts } from '@/lib/mdx'
-import { ArticleBody } from '@/components/ArticleBody'
-import 'highlight.js/styles/atom-one-dark.css'
-import TableOfContents from '@/components/TableOfContent'
+import { getPostBySlug, getPosts } from "@/lib/mdx";
+import { ArticleBody } from "@/components/ArticleBody";
+import "highlight.js/styles/atom-one-dark.css";
+import "highlightjs-copy/styles/highlightjs-copy.css";
+import TableOfContents from "@/components/TableOfContent";
 
 export async function generateStaticParams() {
-    const posts = getPosts(process.env.ARTICLES_PATH)
-    const paths = posts.map((post) => ({
-        slug: post.replace(/\.mdx?$/, ''),
-    }))
+  const posts = getPosts(process.env.ARTICLES_PATH);
+  const paths = posts.map((post) => ({
+    slug: post.replace(/\.mdx?$/, ""),
+  }));
 
-    return paths
+  return paths;
 }
 export async function generateMetadata({ params }, parent) {
-    const { slug } = params
-    const { frontmatter } = await getPostBySlug(slug, process.env.ARTICLES_PATH)
-    const previousImages = (await parent).openGraph?.images || []
+  const { slug } = params;
+  const { frontmatter } = await getPostBySlug(slug, process.env.ARTICLES_PATH);
+  const previousImages = (await parent).openGraph?.images || [];
 
-    return {
-        metadataBase: new URL('https://cristianorrego.dev'),
-        title: `Cristian Orrego Dev 👨‍💻 | ${frontmatter?.title}`,
-        description: frontmatter.description,
-        openGraph: {
-            title: `Cristian Orrego Dev 👨‍💻 | ${frontmatter?.title}`,
-            description: frontmatter.description,
-            images: [frontmatter.cover, ...previousImages],
-            author: 'cristianorregodev',
-        },
-    }
+  return {
+    metadataBase: new URL("https://cristianorrego.dev"),
+    title: `Cristian Orrego Dev 👨‍💻 | ${frontmatter?.title}`,
+    description: frontmatter.description,
+    openGraph: {
+      title: `Cristian Orrego Dev 👨‍💻 | ${frontmatter?.title}`,
+      description: frontmatter.description,
+      images: [frontmatter.cover, ...previousImages],
+      author: "cristianorregodev",
+    },
+  };
 }
 
 export default async function PostPage({ params }) {
-    const { content, frontmatter } = await getPostBySlug(params.slug, process.env.ARTICLES_PATH)
-    return (
-        <>
-            {/* <Banner image={frontmatter?.cover} title={frontmatter?.title} /> */}
-            <ArticleBody content={content} frontmatter={frontmatter} />
-            {/* <TableOfContents /> */}
-        </>
-    )
+  const { content, frontmatter } = await getPostBySlug(
+    params.slug,
+    process.env.ARTICLES_PATH
+  );
+  return (
+    <>
+      {/* <Banner image={frontmatter?.cover} title={frontmatter?.title} /> */}
+      <ArticleBody content={content} frontmatter={frontmatter} />
+      {/* <TableOfContents /> */}
+    </>
+  );
 }
